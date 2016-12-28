0dc56d71773276de1242f57641b4503045be2ca7
devds <oleh.duts@gmail.com>
2016-12-28 11:48:54 +0200

    style for new routing

--- src/post/postController.js
+++ src/post/postController.js
@@ -1,9 +1,5 @@
 app
 
     .controller('PostController', function ($scope) {
-        $(document).ready(function () {
-            $("#tree").jstree({
-                "plugins": ["wholerow"]
-            });
-        });
+
     });
