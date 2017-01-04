4005112c0db64af9f6745e6b1bcf2060fffe7ac6
devds <oleh.duts@gmail.com>
2017-01-04 12:05:59 +0200

    add event

--- src/welcome/welcomeController.js
+++ src/welcome/welcomeController.js
@@ -9,5 +9,12 @@
 app
     .controller('WelcomeController', function ($scope) {
 
-    });
+    })
+    .controller('TimepickerDemoCtrl', function ($scope) {
+    $scope.mytime = new Date();
+
+    $scope.hstep = 1;
+    $scope.mstep = 1;
+
+});
 
