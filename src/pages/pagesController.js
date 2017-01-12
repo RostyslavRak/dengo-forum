c62b06985211473d8b59e705cf6e4144bc52eef0
dim4k1993 <dimaszelenyuk@gmail.com>
2017-01-12 16:20:24 +0200

    create obj Post and edit post and postOne

--- src/pages/pagesController.js
+++ src/pages/pagesController.js
@@ -10,5 +10,25 @@
 app
     .controller('PagesController', function ($scope) {
 
+        // $scope.userObj = [
+        //     {
+        //         id:1,
+        //         name:"Dima",
+        //         lastName:"Zelenyuk",
+        //         photo:"images/src/viawEvents/images/dima.jpg",
+        //         role:"User",
+        //         dateRegistered:"2017-01-12"
+        //     },
+        //     {
+        //         id:2,
+        //         name:"Andrey",
+        //         lastName:"Blizniuk",
+        //         photo:"images/src/viawEvents/images/dima.jpg",
+        //         role:"Admin",
+        //         dateRegistered:"2017-01-22"
+        //     }
+        // ];
+
+
     });
 
