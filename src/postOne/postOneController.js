cf9dd04536557c061d63abfd1d5308ba38223a95
dim4k1993 <dimaszelenyuk@gmail.com>
2017-01-24 18:51:04 +0200

    add post method create comment in add post

--- src/postOne/postOneController.js
+++ src/postOne/postOneController.js
@@ -6,9 +6,35 @@
 
 
 app
-    .controller('PostOneController', function ($scope) {
+    .controller('PostOneController', function ($scope,$rootScope) {
         $scope.maxSize = 5;
         $scope.bigTotalItems = 175;
         $scope.bigCurrentPage = 1;
+
+        $scope.commentsPost = $rootScope.posts[0].comments;
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var m1 = date.getMonth()+1;
+        var y = date.getFullYear();
+        $scope.dateFormat=(y+"-"+m+"-"+d);
+        $scope.dateFormat1_12=(y+"-"+m1+"-"+d);
+
+
+        $scope.newCommentPost = {
+            name:$scope.ls.user.name,
+            photo:$scope.ls.user.photo,
+            data  : $scope.dateFormat1_12,
+            likes:[
+            ]
+
+        };
+
+        $scope.addCommentPost = function () {
+            $scope.commentsPost.push($scope.newCommentPost);
+            $('#commentPost').val("");
+            console.log( $scope.newCommentPost);
+        };
+
 
     });
